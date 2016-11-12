45623325a141c21afd86cc97d68181bb9ef17c44
Jakob Runge <sicarius@g4t3.de>
2016-11-12 18:38:01 +0100

    Add oauthDance.py
    - This is the first successful oauth dance.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import os
 import tweepy
 
+import oauthDance
 
 auth = tweepy.OAuthHandler(
     os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
-auth.set_access_token(
-    os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
 
-api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+if not access_token or not access_token_secret:
+    oauthDance.performOauthDance(auth)
+else:
+    auth.set_access_token(
+        os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
+
+    print('AUTH URL:', auth.get_authorization_url())
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
---
+++ oauthDance.py
@@ -0,0 +1,13 @@
+import tweepy
+
+
+def performOauthDance(auth):
+    print('Please visit:', auth.get_authorization_url())
+    verifier = input('Verification token:')
+    try:
+        auth.get_access_token(verifier)
+        print('Authentication data is:',
+              'ACCESS_TOKEN=', auth.access_token,
+              'ACCESS_TOKEN_SECRET=', auth.access_token_secret)
+    except tweepy.TweepError:
+        print('Failed to authenticate correctly.')
