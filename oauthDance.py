1a821d20d099571db6602965014e8ab9c10c66f4
Jakob Runge <sicarius@g4t3.de>
2016-11-13 20:01:09 +0100

    Improve print in oauthDance.py

--- oauthDance.py
+++ oauthDance.py
@@ -7,8 +7,9 @@ def performOauthDance(auth):
     verifier = input('Verification token:')
     try:
         auth.get_access_token(verifier)
-        print('Authentication data is:',
-              'ACCESS_TOKEN=', auth.access_token,
-              'ACCESS_TOKEN_SECRET=', auth.access_token_secret)
+        print('\n'.join([
+            'Authentication data is:',
+            'ACCESS_TOKEN=%s' % auth.access_token,
+            'ACCESS_TOKEN_SECRET=%s' % auth.access_token_secret]))
     except tweepy.TweepError:
         print('Failed to authenticate correctly.')
